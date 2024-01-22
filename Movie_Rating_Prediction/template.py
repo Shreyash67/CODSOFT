02e408c42a27b89b6ea7e74c9b8b54338b50cf59
SHREYASH WARALKAR <135358294+Shreyash67@users.noreply.github.com>
2024-01-23 00:37:16 +0530

    Update template.py

--- Movie_Rating_Prediction/template.py
+++ Movie_Rating_Prediction/template.py
@@ -4,7 +4,6 @@ from pathlib import Path
 package_name="MovieRatingPrediction"
 
 list_of_files=[
-    "github/workflows/.gitkeep",
     f"src/{package_name}/__init__.py",
     f"src/{package_name}/components/__init__.py",
     f"src/{package_name}/components/data_ingestion.py",
